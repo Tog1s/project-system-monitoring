9c5c7a014de47c82fbb85c502b7d641c8d8a9fe2
Maxim Kursakov <qmax.ku@gmail.com>
2024-03-31 15:29:05 +0300

    add test cases

--- internal/config/config_test.go
+++ internal/config/config_test.go
@@ -7,7 +7,7 @@ import (
 )
 
 func TestConfig(t *testing.T) {
-	t.Run("test ReadFromFile", func(t *testing.T) {
+	t.Run("wrong file in path", func(t *testing.T) {
 		_, err := ReadFromFile("./configs/config.toml")
 		require.Error(t, err)
 	})
--- internal/storage/storage_test.go
+++ internal/storage/storage_test.go
@@ -1,6 +1,7 @@
 package storage
 
 import (
+	"math"
 	"testing"
 	"time"
 
@@ -10,9 +11,9 @@ import (
 	"github.com/tog1s/project-system-monitoring/pkg/loadavg"
 )
 
-func TestWrite(t *testing.T) {
-	t.Run("test write func", func(t *testing.T) {
-		metric := metrics.SystemMetrics{
+func TestStorage(t *testing.T) {
+	t.Run("test write, all, remove", func(t *testing.T) {
+		some_metrics := metrics.SystemMetrics{
 			ID:          uuid.New(),
 			CollectedAt: time.Now(),
 			Load: &loadavg.LoadAverage{
@@ -22,7 +23,70 @@ func TestWrite(t *testing.T) {
 			},
 		}
 		store := New()
-		err := store.Write(metric)
+		err := store.Write(some_metrics)
 		require.NoError(t, err)
+
+		m, err := store.All()
+		require.NoError(t, err)
+		require.Len(t, m, 1)
+		require.Equal(t, some_metrics, m[0])
+
+		err = store.Remove(some_metrics.ID)
+		require.NoError(t, err)
+
+		m, err = store.All()
+		require.NoError(t, err)
+		require.Len(t, m, 0)
+	})
+
+	t.Run("test averaged", func(t *testing.T) {
+		store := New()
+		some_metrics := []metrics.SystemMetrics{
+			{
+				ID:          uuid.New(),
+				CollectedAt: time.Now().Add(-10 * time.Second),
+				Load: &loadavg.LoadAverage{
+					LoadAvg1:  2.0,
+					LoadAvg5:  2.0,
+					LoadAvg15: 2.0,
+				},
+			},
+			{
+				ID:          uuid.New(),
+				CollectedAt: time.Now(),
+				Load: &loadavg.LoadAverage{
+					LoadAvg1:  1.0,
+					LoadAvg5:  1.0,
+					LoadAvg15: 1.0,
+				},
+			},
+			{
+				ID:          uuid.New(),
+				CollectedAt: time.Now(),
+				Load: &loadavg.LoadAverage{
+					LoadAvg1:  1.5,
+					LoadAvg5:  1.5,
+					LoadAvg15: 1.5,
+				},
+			},
+		}
+
+		for _, v := range some_metrics {
+			err := store.Write(v)
+			if err != nil {
+				t.FailNow()
+				return
+			}
+		}
+
+		require.Len(t, store.metrics, 3)
+		avg, err := store.Averaged(5 * time.Second)
+		require.NoError(t, err)
+		require.Len(t, store.metrics, 2)
+
+		require.Equal(t, math.Round(avg.LoadAvg1*100)/100, 1.25)
+		require.Equal(t, math.Round(avg.LoadAvg5*100)/100, 1.25)
+		require.Equal(t, math.Round(avg.LoadAvg15*100)/100, 1.25)
+
 	})
 }
